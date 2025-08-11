aec685935a0863ce04816b8e03ff8e388437fe6e
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-08-11 17:00:50 +0100

    delete

--- main.tf
+++ main.tf
@@ -10,23 +10,23 @@ module "vpc" {
 
     }
 
-    # module "ec2"{
-    #     source = "git::https://github.com/KokBoakye/terraform-aws-modules.git//modules/ec2?ref=main"
+    module "ec2"{
+        source = "git::https://github.com/KokBoakye/terraform-aws-modules.git//modules/ec2?ref=main"
 
-    #     instance_type = var.instance_type
-    #     key_name = var.key_name
-    #     ami_id = var.instance_ami_id
-    #     subnet_id = module.vpc.public_subnet_id
-    #     security_group_ids = [module.vpc.web_sg_id]
-    #     vpc_id = module.vpc.vpc_id
-    # }
+        instance_type = var.instance_type
+        key_name = var.key_name
+        ami_id = var.instance_ami_id
+        subnet_id = module.vpc.public_subnet_id
+        security_group_ids = [module.vpc.web_sg_id]
+        vpc_id = module.vpc.vpc_id
+    }
 
-    #  module "security_group"{
-    #     source = "git::https://github.com/KokBoakye/terraform-aws-modules.git//modules/security_group?ref=main"
+     module "security_group"{
+        source = "git::https://github.com/KokBoakye/terraform-aws-modules.git//modules/security_group?ref=main"
 
-    #     app_port = var.app_port
-    #     security_group_name = var.security_group_name
-    #     subnet_id = module.vpc.public_subnet_id
-    #     security_group_ids = [module.vpc.web_sg_id]
-    #     vpc_id = module.vpc.vpc_id
-    # }
+        app_port = var.app_port
+        security_group_name = var.security_group_name
+        subnet_id = module.vpc.public_subnet_id
+        security_group_ids = [module.vpc.web_sg_id]
+        vpc_id = module.vpc.vpc_id
+    }
