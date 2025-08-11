edb6a2bf7ccb283047b14fde7360f498aaacb9d4
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-08-11 16:20:06 +0100

    upload

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ module "vpc" {
     vpc_cidr_block = var.vpc_cidr_block
     public_subnet_cidr_block = var.public_subnet_cidr_block
     private_subnet_cidr_block = var.private_subnet_cidr_block
-    availability_zones = var.availability_zones
+
 
     }
 
