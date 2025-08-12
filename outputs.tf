2aa99c5d00a04de21b51ee93c9cc19cbfe32034f
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-08-12 15:05:30 +0100

    instance id variable passed

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,12 @@ output "vpc_id" {
   
 }
 
+output "web_server_instance_ids" {
+  description = "IDs of the EC2 instances"
+  value       = module.ec2.web_server_instance_ids
+}
+
+output "public_ips" {
+  description = "Public IPs of the EC2 instances"
+  value       = module.ec2.public_ip
+}
