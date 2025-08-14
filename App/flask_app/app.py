2a2fef23960ff3b3b82b4259abfc1c86db8722f6
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-08-14 10:51:33 +0100

    flask app configure

--- App/flask_app/app.py
+++ App/flask_app/app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 def get_db_connection():
     conn = psycopg2.connect(
         host=os.getenv('DB_HOST'),
-        database='appdb',
+        database=os.getenv('DB_NAME', 'appdb'),
         user=os.getenv('DB_USER'),
-        password=os.getenv('DB_PASSWORD'),
+        password=os.getenv('DB_PASSWORD')
     )
     return conn
 
