558f8d0334eeaa8d8d2b6957537a7c4684591d8f
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-08-14 20:22:06 +0100

    inserted tables

--- App/flask_app/app.py
+++ App/flask_app/app.py
@@ -4,21 +4,24 @@ import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    conn = psycopg2.connect(
-        host=os.environ.get("DB_HOST"),
-        user=os.environ.get("DB_USER"),
-        password=os.environ.get("DB_PASSWORD"),
-        dbname=os.environ.get("DB_NAME")
+def get_db_connection():
+    return psycopg2.connect(
+        host=os.getenv('DB_HOST'),
+        database=os.getenv('DB_NAME'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        sslmode='require'
     )
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute("SELECT NOW()")
-    db_time = cur.fetchone()[0]
+    cur.execute('SELECT id, name, email FROM users;')
+    users = cur.fetchall()
     cur.close()
     conn.close()
+    return render_template('users.html', users=users)
 
-    return render_template("index.html", db_time=db_time)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
