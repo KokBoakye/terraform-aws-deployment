ea6ae4ed24ca83b1c4bfa52c8186e4a046bb1e6d
KokBoakye <133584570+KokBoakye@users.noreply.github.com>
2025-08-21 20:23:59 +0100

    alb fix

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -7,8 +7,7 @@ module "vpc" {
     private_subnet_cidr_block = var.private_subnet_cidr_block[*]
     public_subnet_cidr_block = var.public_subnet_cidr_block
     user = var.user
-    alb_sg = module.security-group.alb_sg_id
-    instance_id = module.ec2.instance_id
+    
 
 }
 
@@ -37,3 +36,43 @@ module "security-group" {
    
 }
 
+resource "aws_lb" "app_alb" {
+  name               = "${var.environment}-${var.user}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [module.security-group.alb_sg_id]
+  subnets           = [aws_subnet.public_subnet.id]
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  name     = "${var.environment}-${var.user}-app-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+
+  health_check {
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 10
+    path                = "/"
+  }
+  
+}
+
+resource "aws_lb_listener" "app_listener" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "group_attachment" {
+  target_group_arn = aws_lb_target_group.app_tg.arn
+  target_id        = module.ec2.instance_id  # string
+  port             = 80
+}
